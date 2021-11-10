dee57d4145c3446dee79bf7a779f8fc38298b472
platypusmt <54313903+platypusmt@users.noreply.github.com>
2021-11-10 01:51:46 -0300

    flutter-app-simple-form
    Flutter APP para conhecer fundamentos em um formulário simples.

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Raxa Conta"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Icon(
+                  Icons.monetization_on,
+                  size: 120,
+                  color: Colors.blue,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Quantidade de pessoas",
+                      labelStyle: TextStyle(color: Colors.blue)),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.blue, fontSize: 18),
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Valor da conta",
+                      labelStyle: TextStyle(color: Colors.blue)),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.blue, fontSize: 18),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20,bottom: 20),
+                  child: Container(
+                    height: 40,
+                    child: RaisedButton(
+                            onPressed: () {},
+                            color: Colors.blue,
+                            child: Text("Calcular", style: TextStyle(color: Colors.white, fontSize: 18),),
+                          ),
+                  ),
+                ),
+                Text(
+                  "Valor por pessoa",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.blue, fontSize: 18),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
